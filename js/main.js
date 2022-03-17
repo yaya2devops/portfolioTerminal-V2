7c26fbd118b891e2ba1df5ff13fa6afe7e2d5e9e
Y4HYA4 <yahia.aboulhaj@istic.ucar.tn>
2022-03-17 15:48:31 +0100

    commit

---
+++ js/main.js
@@ -0,0 +1,69 @@
+const about = document.querySelector('#about')
+const contact = document.querySelector('#contact')
+const blog = document.querySelector('#blog')
+const aboutContent = document.querySelector('#about-content')
+const contactContent = document.querySelector('#contact-content')
+const blogContent = document.querySelector('#blog-content')
+
+about.addEventListener('click', () => {
+  const aboutBox = new WinBox({
+    title: 'About Me',
+    // modal: true,
+    width: '400px',
+    height: '400px',
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: aboutContent,
+    onfocus: function () {
+      this.setBackground('#00aa00')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
+  })
+})
+
+contact.addEventListener('click', () => {
+  const contactBox = new WinBox({
+    title: 'Contact Me',
+    background: '#00aa00',
+    width: '400px',
+    height: '400px',
+    top: 150,
+    right: 50,
+    bottom: 50,
+    left: 250,
+    mount: contactContent,
+    onfocus: function () {
+      this.setBackground('#00aa00')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
+  })
+})
+
+
+blog.addEventListener('click', () => {
+  const blogBox = new WinBox({
+    title: 'Blogs',
+    background: '#00aa00',
+    width: '400px',
+    height: '400px',
+    top: 150,
+    right: 50,
+    bottom: 50,
+    left: 250,
+    mount: blogContent,
+    onfocus: function () {
+      this.setBackground('#00aa00')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
+  })
+})
+
+//Reading RSS feed for blogs
